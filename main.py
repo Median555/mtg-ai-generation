2f1097916b2fbb1d3a490fc8f99576f010dac9ec
Mikkel Pilegaard <mikkel.median@gmail.com>
2023-03-12 15:10:23 +0100

    support for multiple synergistic cards generated at the same time

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sys
 import os
 import zipfile
 import io
@@ -11,9 +10,43 @@ from base64 import b64decode
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-theme = 'cow tribal'
 
-prompt = f'''Could you generate a new magic card? It should have a theme around "{theme}". Output it as json and use this template:
+def get_card(response):
+	card = None
+	try:
+		card = json.loads(response)
+	except json.JSONDecodeError:
+		# Try to extract some json
+		possible_json = response[response.find('{') : response.rfind('}')+1]
+		try:
+			card = json.loads(possible_json)
+		except json.JSONDecodeError:
+			pass
+	# We failed to parse the card, return to caller to determine next step
+	if not card:
+		return None
+	
+	# Do some cleaun
+	# Coallesce
+	card['super type'] = ' '.join([t.capitalize() for t in card['types']])
+	card['sub type'] = ' '.join([t.capitalize() for t in card.get('subtypes', '')])
+
+	# Format text
+	indent_prefix = '\n\t\t'
+	card['text'] = indent_prefix.join([line.strip() for line in card['text']])
+	card['text'] = card['text'].replace('{', '<sym>').replace('}', '</sym>').replace('~', f'<atom-cardname><nospellcheck>{card["name"]}</nospellcheck></atom-cardname>')
+	
+	card['flavor'] = indent_prefix.join(card['flavor'].split('\n'))
+
+	return card
+
+
+def generate_cards(theme, nums=1):
+	assert nums >= 1
+
+	out = list()
+
+	start_prompt = f'''Could you generate a new magic card? It should have a theme around "{theme}". Output it as json and use this template:
 ```json
 {{
 	"name": <name>,
@@ -29,54 +62,37 @@ prompt = f'''Could you generate a new magic card? It should have a theme around 
 }}
 ```'''
 
-# Could you make another card with the same theme that synergizes with that?
-# Could you build deck using those cards and any existing relevant magic cards?
+	thread = [{"role": "user", "content": start_prompt}]
 
-card = None
+	# Could you build deck using those cards and any existing relevant magic cards?
 
-while not card:
-	response = openai.ChatCompletion.create(
-	model="gpt-3.5-turbo",
-	max_tokens=400,
-	messages=[
-			{"role": "user", "content": prompt},
-		]
-	)
-	response = str(response['choices'][0]['message']['content'])
-	try:
-		card = json.loads(response)
-		break
-	except json.JSONDecodeError:
-		# Try to extract some json
-		possible_json = response[response.find('```json')+7 : response.rfind('```')]
-		try:
-			card = json.loads(possible_json)
-			break
-		except json.JSONDecodeError:
-			print('GPT gave us something we did not understand:', response)
-			print('Trying again')
-	
 	# TODO: Ask GPT to correct cards if not quite right, like:
 	#  non-creatures having power and toughness
 	#  creatures having no power and toughness
 	#  enchantments having sorcery/instant like abilities (how to check this?)
+	for i in range(1, nums+1):
+		card = None
+		while not card:
+			response = openai.ChatCompletion.create(
+				model="gpt-3.5-turbo",
+				max_tokens=400,
+				messages=thread
+			)
+			response = str(response['choices'][0]['message']['content'])
+			card = get_card(response)
+			if not card: # Did we get a valid card
+				print('GPT gave us something we did not understand:', response)
+				print('Trying again')
+			else:
+				out.append(card)
+				thread.append({'role': 'assistant', 'content': response})
+				thread.append({'role': 'user', 'content': 'Could you make another card with the same theme that synergizes with that?'})
+				print(thread)
+	
+	return out
 
-print(card)
-
-# Coallesce
-card['super type'] = ' '.join([t.capitalize() for t in card['types']])
-card['sub type'] = ' '.join([t.capitalize() for t in card.get('subtypes', '')])
-
-# Format text
-indent_prefix = '\n\t\t'
-card['text'] = indent_prefix.join([line.strip() for line in card['text']])
-card['text'] = card['text'].replace('{', '<sym>').replace('}', '</sym>').replace('~', f'<atom-cardname><nospellcheck>{card["name"]}</nospellcheck></atom-cardname>')
-#
-card['flavor'] = indent_prefix.join(card['flavor'].split('\n'))
-
-
-raw_set_file = f'''
-mse_version: 2.0.2
+def render_card(card):
+	raw_set_file = f'''mse_version: 2.0.2
 game: magic
 game_version: 2020-04-25
 stylesheet: m15-altered
@@ -113,36 +129,43 @@ version_control:
 apprentice_code: 
 '''
 
-#print(raw_set_file)
-timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+	timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 
-response = openai.Image.create(
-	prompt=f'modern magic the gathering card art depicting a {card["super type"]} {card["sub type"]} named "{card["name"]}", {card["image_desc"]}, digital art, art station, 4k render',
-	n=1,
-	size="512x512",
-	response_format="b64_json"
-)
+	response = openai.Image.create(
+		prompt=f'modern magic the gathering art depicting a {card["super type"]} {card["sub type"]} named "{card["name"]}", {card["image_desc"]}, digital art, art station, 4k render',
+		n=1,
+		size="512x512",
+		response_format="b64_json"
+	)
 
-os.makedirs('./output/art', exist_ok=True)
-os.makedirs('./output/sets', exist_ok=True)
+	os.makedirs('./output/art', exist_ok=True)
+	os.makedirs('./output/sets', exist_ok=True)
+	card_output_path = f'../output/{timestamp}.png'
 
-# Save art separately
-with open(f'./output/art/{timestamp}-art.png', 'wb') as f:
-	f.write(b64decode(response['data'][0]['b64_json']))
+	# Save art separately
+	with open(f'./output/art/{timestamp}-art.png', 'wb') as f:
+		f.write(b64decode(response['data'][0]['b64_json']))
 
-# Write set file
-buf = io.BytesIO()
-with zipfile.ZipFile(buf, 'x', zipfile.ZIP_DEFLATED) as f:
-	f.writestr('set', str(raw_set_file))
-	f.writestr('image1', b64decode(response['data'][0]['b64_json']))
-with open(f'./output/sets/{timestamp}.mse-set', 'wb') as f:
-	f.write(buf.getvalue())
+	# Write set file
+	buf = io.BytesIO()
+	with zipfile.ZipFile(buf, 'x', zipfile.ZIP_DEFLATED) as f:
+		f.writestr('set', str(raw_set_file))
+		f.writestr('image1', b64decode(response['data'][0]['b64_json']))
+	with open(f'./output/sets/{timestamp}.mse-set', 'wb') as f:
+		f.write(buf.getvalue())
 
-# Generate card from set file
-p = subprocess.Popen(f'mse.exe --cli --raw ../output/sets/{timestamp}.mse-set',
-	cwd="mse", shell=True,
-	stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-p.stdin.write(str.encode(f'write_image_file(set.cards.0, file: "../output/{timestamp}-{card["name"]}.png")'))
-p.stdin.write(str.encode(f'\nwrite_image_file(set.cards.0, file: "../newest-card.png")'))
-print(p.communicate(timeout=15)[0])
-p.stdin.close()
+	# Generate card from set file
+	p = subprocess.Popen(f'mse.exe --cli --raw ../output/sets/{timestamp}.mse-set',
+		cwd="mse", shell=True,
+		stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+	p.stdin.write(str.encode(f'write_image_file(set.cards.0, file: "{card_output_path}")'))
+	p.stdin.write(str.encode(f'\nwrite_image_file(set.cards.0, file: "../newest-card.png")'))
+	print(p.communicate(timeout=15)[0])
+	p.stdin.close()
+
+	return card_output_path
+
+theme ='cow tribal'
+cards = generate_cards(theme, 2)
+for card in cards:
+	print(render_card(card), card)
